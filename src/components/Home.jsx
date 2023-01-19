70dfe6cd13bee844a31d8589b9bbac16c4227044
Idriss Garfa <idrissgarfa@gmail.com>
2023-01-19 12:30:09 +0100

    Initial animation state

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
+
 const Home = () => {
   return (
     <motion.div className="home container"
-      animate={{ opacity: 0.8, marginTop: 200 }}
+      initial={{ opacity: 0}}
+      animate={{ opacity: 1}}
     >
-      <motion.h2 
-        animate={{ fontSize: '40px', color: '#ff2994', x: 100, y: -100}}
+      <h2 
       >
         Welcome to Pizza Joint
-      </motion.h2>
+      </h2>
       <Link to="/base">
         <motion.button 
           animate={{ scale: 1.3 }}
